b5daee434ab4e2897b32bcd0b3984e04be2a89b9
shivansh <shivresides@gmail.com>
2025-06-11 15:23:17 +0530

    first commit

---
+++ Group-Anagram/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream> 
+
+
+//this is my 9th leetcode PROBLEM 
+
+//MY FIRST APPROACH I HAVE in steps are;
+
+/*
+1. Sort the strings in the VECTOR 
+
+2. STORE IT IN A DATA STRUCTURE, I WILL USE UNORDERED MAP
+
+3. ITERATE OVER IT, FIND THE SAME STRINGS 
+
+4. STORE THEM AGAIN, RETURN THE VECTOR 
+
+5. PUSH IT TO THE GLOBAL VECTOR
+*/
+
+int main(){
+    std::cout << "hello world";
+    return 0;
+}
